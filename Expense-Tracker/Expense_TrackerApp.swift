6b7a5413099df576e2f4f0653ad54f89d2a40efe
TwelveStudent47 <laczkokevin60@gmail.com>
2025-06-19 10:45:19 +0200

    Initial Commit

---
+++ Expense-Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense-Tracker
+//
+//  Created by Laczkó Kevin on 2025. 06. 19..
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Expense-TrackerTests/Expense_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerTests.swift
+//  Expense-TrackerTests
+//
+//  Created by Laczkó Kevin on 2025. 06. 19..
+//
+
+import Testing
+@testable import Expense_Tracker
+
+struct Expense_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
